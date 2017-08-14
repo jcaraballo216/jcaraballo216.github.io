c13df1bc913b9ea3b9505a9baf7ba1fbef8986dc
Jerome <jscar216@gmail.com>
2017-08-14 00:47:20 -0400

    made final touches

--- startbootstrap-freelancer-gh-pages/mail/contact_me.php
+++ startbootstrap-freelancer-gh-pages/mail/contact_me.php
@@ -1,22 +1,23 @@
+
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_post['name'])  		||
+   empty($_post['email']) 		||
+   empty($_post['phone']) 		||
+   empty($_post['message'])	||
+   !filter_var($_post['email'],filter_validate_email))
    {
-	echo "No arguments Provided!";
+	echo "no arguments provided!";
 	return false;
    }
-	
-$name = strip_tags(htmlspecialchars($_POST['name']));
+
+$name = strip_tags(htmlspecialchars($_post['name']));
 $email_address = strip_tags(htmlspecialchars($_POST['email']));
 $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 	
 // Create the email and send the message
-$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'jscar216@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
 $headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
